7e318e42d68e8a9b2ee7a58ecdd487e91f725784
Sebastian Guido <sebastianezequielguido@gmail.com>
2021-12-11 16:14:56 -0300

    comments + README update

--- src/exclude.js
+++ src/exclude.js
@@ -5,6 +5,7 @@ const { getLpBifiData, getMooBifiBoostAddresses, getLpBifiBoostedData, getStrate
 
 var excludedAddresses = new Set();
 
+//Load all excluded lists
 const loadExcludedAddresses = async () => {
 
     Object.values(blacklist).forEach(address => {
@@ -12,17 +13,22 @@ const loadExcludedAddresses = async () => {
     });
 
     for (const chain of Object.values(chains)) {
-        excludedAddresses.add(chain.bifi.toLocaleLowerCase());
         excludedAddresses.add(chain.maxi.toLocaleLowerCase());
         excludedAddresses.add(chain.rewards.toLocaleLowerCase());
 
+        //Add Vault addresses
         let lps = await getLpBifiData(chain);
         lps.forEach(lp => excludedAddresses.add(lp.address.toLocaleLowerCase()));
+
+        //Add mooBifi boost addresses
         let boosts = await getMooBifiBoostAddresses(chain);
         boosts.forEach(boost => excludedAddresses.add(boost.toLocaleLowerCase()));
+
+        //Add BIFI vault boost addresses
         let lpBoosts = await getLpBifiBoostedData(chain);
         lpBoosts.forEach(lpBoost => excludedAddresses.add(lpBoost.address.toLocaleLowerCase()));
 
+        //Add strategy addresses fir BIFI vaults + Maxi
         const vaultAddresses = lps.map(lp => lp.address);
         let strategies = await getStrategyAddressForVaults([...vaultAddresses, chain.maxi], chain);
         strategies.forEach(strat => excludedAddresses.add(strat.toLocaleLowerCase()));
